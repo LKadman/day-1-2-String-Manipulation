eedae4900a0be4ee66de5b04e8dce2d2a22d05da
Lot Kaduma <42066033+LKadman@users.noreply.github.com>
2022-10-15 11:11:07 +0000

    Exercise 2 - Debugging Practice

--- main.py
+++ main.py
@@ -67,3 +67,25 @@ print("Hello" + " " + "Angela")
 # Means to remove broken code
 # In the 1980s a moth flew into one of the early computers and got electricuted which stopped the program from working. It started working again after it was 'debugged' - the bug was removed and the wires were fixed
 
+# Exercise 2 - Debugging Practice
+# Fix the code below 👇
+
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+
+
+#Instructions
+# Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+
+# Warning: The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+
+# Example Output
+# When you run your program, it should print the following:
+
+#Day 1 - String Manipulation
+#String Concatenation is done with the "+" sign.
+#e.g. print("Hello " + "world")
+#New lines can be created with a backslash and n.
