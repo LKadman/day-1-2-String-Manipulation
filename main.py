952d0da75c10c60aab6e7a35e7fa2c13e0aafe29
Lot Kaduma <42066033+LKadman@users.noreply.github.com>
2022-10-15 09:28:25 +0000

    Output multiple lines in a single code

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+# String Manipulation
+
+# This lesson explains how to organise code in a much efficient way
+
+# 1.0 Output multiple lines in a single line of code using (\n) within the code. Note: use a backslash (\) not a frontslash (/)
+
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+
+print('Hello World!\n Hello World!\n  Hello World!')
+
+# Use three quotation marks to run multiple lines in input without the need for the print() function in each line
+
+print('''Hello World 
+  Hello World
+    Hello World''')
+
+# 
 #Fix the code below 👇
 
-print(Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-  print('e.g. print("Hello " + "world")')
-print(("New lines can be created with a backslash and n.")
+#print(Day 1 - String Manipulation")
+#print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+#print(("New lines can be created with a backslash and n.")
 
 
