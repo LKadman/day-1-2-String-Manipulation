1e34ffd12b9f491669397f540556fdc6621adb1d
Lot Kaduma <42066033+LKadman@users.noreply.github.com>
2022-10-15 09:57:05 +0000

    Concatenation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # This lesson explains how to organise code in a much efficient way
 
-# 1.0 Output multiple lines in a single line of code using (\n) within the code. Note: use a backslash (\) not a frontslash (/)
+# 1.0 Line Management
+# Output multiple lines in a single line of code using (\n) within the code. Note: use a backslash (\) not a frontslash (/)
 
 print('Hello World!')
 print('Hello World!')
@@ -16,12 +17,20 @@ print('''Hello World
   Hello World
     Hello World''')
 
-# 
+# 2.0 Concatenation
+# To combine different strings so that they can be added to another string by using the (+) sign between strings
+
+print("Hello" + "Angela")
+# to separate them, add a space after Hello or before Angela - all within each string
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+# or concatenate another string with an empty space (" ") in between the strings
+print("Hello" + " " + "Angela")
+#Note: If we think of Strings like a 'string' merging several characters together, then concatenation is the merging of several Strings together
+
 #Fix the code below 👇
 
 #print(Day 1 - String Manipulation")
 #print("String Concatenation is done with the "+" sign.")
 #  print('e.g. print("Hello " + "world")')
 #print(("New lines can be created with a backslash and n.")
-
-
